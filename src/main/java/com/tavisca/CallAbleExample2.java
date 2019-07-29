cb63e89c04044c8244ad34915f702e146b9e2eea
tavisca-ksingh <ksingh@tavisca.com>
2019-07-29 14:09:33 +0530

    update file

--- src/main/java/com/tavisca/CallAbleExample2.java
+++ src/main/java/com/tavisca/CallAbleExample2.java
@@ -5,6 +5,7 @@ import javax.xml.ws.Service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.*;
+import java.util.function.Function;
 
 public class CallAbleExample2 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
@@ -16,6 +17,21 @@ public class CallAbleExample2 {
             Future<String> future = service.submit(call);
             list.add(future);
         }
+        //functional interface
+        Function<Future<String>, String> function = (f) ->{
+            String name = "";
+            try {
+               name =  f.get();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }catch (ExecutionException e){
+                e.printStackTrace();
+            }
+            return name;
+        };
+
+
+        list.stream().map(function).forEach(System.out::println);
         for (Future<String> f : list){
             System.out.println(System.currentTimeMillis() +" ::: " + f.get());
         }
