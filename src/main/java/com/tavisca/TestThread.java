7ef54769bb94bc9b8755b41800f19c14bea29c91
tavisca-ksingh <ksingh@tavisca.com>
2019-07-30 00:23:51 +0530

    mutlithreading

---
+++ src/main/java/com/tavisca/TestThread.java
@@ -0,0 +1,18 @@
+package com.tavisca;
+
+class TestThread extends Thread{
+    public void run(){
+        System.out.println("running thread name is:"+Thread.currentThread().getName());
+        System.out.println("running thread priority is:"+Thread.currentThread().getPriority());
+
+    }
+    public static void main(String args[]){
+        TestThread m1=new TestThread();
+        TestThread m2=new TestThread();
+        m1.setPriority(Thread.MIN_PRIORITY);
+        m2.setPriority(Thread.MAX_PRIORITY);
+        m1.start();
+        m2.start();
+
+    }
+}
